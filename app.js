d07fe1df3d09ee4690115323689d168dd4ba023c
holy-script <palash.18@intl.zju.edu.cn>
2021-05-16 03:39:21 +0530

    Firebase & init commands set, input cleaned

--- app.js
+++ app.js
@@ -1,18 +1,90 @@
 "use strict";
 const Discord = require("discord.js");
 require("dotenv").config();
+const Firestore = require("@google-cloud/firestore");
+
+const db = new Firestore({
+    projectId: process.env.PROJECT_ID,
+    keyFilename: process.env.KEY_FILE_PATH,
+});
+const projectRef = db.collection("projects");
 
 const client = new Discord.Client();
 
+const currentProject = {
+    name: "",
+    token_v2: "",
+    pageLink: "",
+    admin: "",
+};
 const commandPallette = {
-    init: async() => {
-        return "Project initialized.";
+    init: {
+        setup: async(params) => {
+            userId = params.pop();
+            projectName = params.join(" ");
+            await projectRef.doc(projectName).set({
+                token_v2: "",
+                pageLink: "",
+                admin: userId,
+            });
+            currentProject.name = projectName;
+            currentProject.admin = userId;
+            return "Project successfully setup!";
+        },
+        load: async(params) => {
+            const user = params.pop();
+            const old = currentProject.admin;
+            let toSet = await projectRef.doc(params.join(" ")).get();
+            if (toSet.exists) {
+                currentProject.name = params.join(" ");
+                toSet = toSet.data();
+                Object.keys(toSet).forEach((key) => (currentProject[key] = toSet[key]));
+                return `Project successfully loaded ${user}, please check status.\nAdministrator changed from ${
+          old || "None"
+        } to ${currentProject.admin}!`;
+            } else return "Project not found!";
+        },
+        menu: async(params) => {
+            const user = params.pop();
+            return `Hello ${user}, this is the menu!`;
+        },
     },
-    admin: async() => {
-        return "Administrator command used";
+    admin: {
+        token: async(params) => {
+            return `The current token value has been set!`;
+        },
+        page: async(params) => {
+            return `The current page link has been set!`;
+        },
+        lock: async(params) => {
+            return "Page Locked!";
+        },
+        history: async(params) => {
+            return "Science, yo! No, wait...";
+        },
     },
-    crud: async() => {
-        return "CRUD operation performed";
+    crud: {
+        add: async(params) => {
+            return "Add used";
+        },
+        read: async(params) => {
+            return "Read used";
+        },
+        update: async() => {
+            return "Update used";
+        },
+        delete: async() => {
+            return "Delete used";
+        },
+    },
+    status: async() => {
+        return currentProject.name ?
+            `\`\`\`Project: ${currentProject.name}\n\nToken: ${
+          currentProject.token_v2 ? "Set" : "Not Set"
+        }\n\nPage: ${currentProject.pageLink || "Not Set"}\n\nAdmin: ${
+          currentProject.admin
+        }\`\`\`` :
+            "Please setup the project first!";
     },
 };
 
@@ -21,8 +93,44 @@ client.on("ready", async() => {
 });
 
 client.on("message", async(msg) => {
-    if (msg.content === "ping") {
-        msg.reply("pong");
+    if (!msg.author.bot) {
+        if (msg.attachments.size > 0) {
+            msg.attachments.forEach((attachment) => {
+                quickstart(attachment.url);
+            });
+        }
+        let command = msg.content.trim();
+        let params = command.split(/\s+/);
+        command = command.match(/\![a-zA-Z]+/);
+        command !== null ?
+            (command = command[0].slice(1)) :
+            msg.channel.send("Did not find any command, please try again");
+        params.length > 1 ?
+            params.splice(params.indexOf(`!${command}`), 1) :
+            (params = null);
+        if (!params) {
+            command === "status" ?
+                msg.channel.send(await commandPallette[command]()) :
+                msg.reply("Please add proper command parameters!");
+        } else {
+            if (commandPallette.init.hasOwnProperty(command)) {
+                msg.channel.send(
+                    await commandPallette.init[command]([...params, msg.author.username])
+                );
+            } else if (commandPallette.admin.hasOwnProperty(command)) {
+                currentProject.admin === msg.author.username ?
+                    msg.channel.send(await commandPallette.admin[command](params)) :
+                    msg.reply("You are not authorized, please request project admin.");
+            } else if (commandPallette.crud.hasOwnProperty(command)) {
+                currentProject.token_v2 && currentProject.pageLink ?
+                    msg.channel.send(await commandPallette.crud[command](params)) :
+                    msg.reply(
+                        "Please set the token and page link before performing this operation."
+                    );
+            } else {
+                msg.reply("Invalid command, please type `!menu`");
+            }
+        }
     }
 });
 
